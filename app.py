d76cfa7a879fb261daabc5b69e08c2a3c77b57a7
Camacho <rafaelcamachofitti15@gmail.com>
2024-05-23 19:46:58 -0300

    Variaveis globais e Requisição dos forms

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 import streamlit as st
 import pandas as pd
+from requests.auth import HTTPBasicAuth
+import requests
+
+#Variaveis Globais
+API_KEY = 'Rm9ybUFwaUZlaXRhUGVsb0plYW5QaWVycmVQYXJhYURlc2Vudm9sdmU='
+
+IDS = {'ITABIRA': '5b91aec2-e7ae-45e8-8146-bb7e5c40a8b6',
+       'BOM DESPACHO': 'b4c0bf14-2472-4f48-ae5e-5c16e5e592ed',
+       'PEQUI': 'a09d7656-f2a0-4b33-8c12-c8a4580e5e9d'}
+
+#Requisição
+url  = 'http://api-hml.pdcloud.dev/' 
+headers = {'api-key': API_KEY}
+endpoint = f"enrolled/city/{IDS['ITABIRA']}"
+req = requests.get(url+endpoint, headers=headers)
+res = req.json()
+print(res)
 
 st.title('Dados do Processo Seletivo')
-
-# Load data
-
